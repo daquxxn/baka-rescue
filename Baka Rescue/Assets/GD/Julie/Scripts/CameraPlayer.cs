cc64ddbdc36a46b374a76d2053f88716b82c250f
daquxxn <50579443+daquxxn@users.noreply.github.com>
2020-04-19 04:23:49 +0200

    Prog moving charac

---
+++ Baka Rescue/Assets/GD/Julie/Scripts/CameraPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour
+{
+    [SerializeField] private Camera _mainCamera = null;
+    [SerializeField] private Transform _charac = null;
+    private Transform _cameraTrans = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraTrans = _mainCamera.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _cameraTrans.position = new Vector3(_charac.position.x, _cameraTrans.position.y, _cameraTrans.position.z);
+    }
+}
---
+++ Baka Rescue/Assets/GD/Julie/Scripts/CharacController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacController : MonoBehaviour
+{
+    [SerializeField] private int _speedCharac = 10;
+    [SerializeField] private Transform _charaTrans = null;
+    [SerializeField] private Rigidbody _rb = null;
+    [SerializeField] private int _jumpForce = 0;
+    private bool _isGrounded = false;
+    [SerializeField] private float _rayDistance = 0;
+    [SerializeField] private LayerMask _ground = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newVelocity = _rb.velocity;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 newDirection = new Vector3(moveHorizontal, 0, 0);
+
+        if (moveHorizontal != 0)
+        {
+            newVelocity.x = moveHorizontal * _speedCharac * Time.deltaTime;
+            _charaTrans.transform.right = newDirection;
+        }
+        
+
+        RaycastHit raycastHit;
+
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, out raycastHit, _rayDistance, _ground);
+
+        if (_isGrounded && Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("juju");
+            newVelocity.y = _jumpForce;
+        }
+        if (newVelocity != Vector3.zero)
+            _rb.velocity = newVelocity;
+    }
+}
