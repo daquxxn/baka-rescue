5b471945af11f4d21b521a1d7c780844ce418f32
daquxxn <50579443+daquxxn@users.noreply.github.com>
2020-05-07 12:42:55 +0200

    collectble barre de vie

---
+++ Baka Rescue/Assets/GD/Julie/Scripts/Collectible/Collectibles.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    [SerializeField] private CharacController _characController = null;
+    [SerializeField] private Healthbar _healthBar = null;
+
+    [SerializeField] private int _lifeHealed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameObject.SetActive(false);
+        if(_characController.CurrentHealth<100)
+        GetLifeBack(_lifeHealed);
+        
+    }
+
+    private void GetLifeBack(int heal)
+    {
+      _characController.CurrentHealth += heal;
+     _healthBar.SetHealth(_characController.CurrentHealth);
+    }
+}
