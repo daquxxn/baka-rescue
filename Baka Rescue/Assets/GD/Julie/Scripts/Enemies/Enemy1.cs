dfeb4ea4e19d7125cd436edafa5abb92ae36bd6b
daquxxn <50579443+daquxxn@users.noreply.github.com>
2020-05-22 12:21:27 +0200

    r

--- Baka Rescue/Assets/GD/Julie/Scripts/Enemies/Enemy1.cs
+++ Baka Rescue/Assets/GD/Julie/Scripts/Enemies/Enemy1.cs
@@ -6,12 +6,21 @@ public class Enemy1 : BaseEnemy
 {
     [SerializeField] private int _rotationSpeed = 10;
     [SerializeField] private Transform _trans = null;
+    [SerializeField] private int _dir = -1; 
+    [SerializeField] private float _clampRotation = 0f; 
 
     
 
     private void Update()
     {
-        if (_trans.rotation.x <= 180)
-    _trans.transform.Rotate(Vector3.right * (_rotationSpeed * Time.deltaTime));
+        if(_dir == 1 && _trans.rotation.x >= _clampRotation/1000)
+         {
+             _dir = -1;
+         }
+         else if(_trans.rotation.x <= -_clampRotation/1000)
+         {
+             _dir = 1;
+         }
+        _trans.transform.Rotate(Vector3.right * (_rotationSpeed * _dir * Time.deltaTime));
     }
 }
